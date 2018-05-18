eb9cf7d5586690e0d22f5f9e049a45d031da40d6
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-18 20:34:09 +0900

    Remove sum and product function

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -6,6 +6,7 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
+  /*
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
@@ -16,6 +17,7 @@ object List {
     case Cons(0.0, _) => 0.0
     case Cons(x, xs) => x * product(xs)
   }
+  */
 
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
