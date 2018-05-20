2c94ae492a21ded6a7a448a2736b44ff53f4e4c9
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-20 20:21:39 +0900

    Fix List 3-4

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -206,6 +206,6 @@ object List {
     }
 
   sealed trait Tree[+A]
-  case class Left[A](value: A) extends Tree[A]
+  case class Leaf[A](value: A) extends Tree[A]
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 }
