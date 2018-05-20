739b1920029fe779e5ec7f9d0f66a1903de7ca3b
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-20 23:50:38 +0900

    Add initialise code for chapter 4

---
+++ src/test/scala/fpinscala/errorhandling/OptionSpec.scala
@@ -0,0 +1,24 @@
+package fpinscala.errorhandling
+
+import org.scalatest.FunSuite
+
+class OptionSpec extends FunSuite {
+
+  // test("3-2 tail") {
+    // assertThrows[RuntimeException] {
+      // List.tail(Nil)
+    // }
+    // assert(List.tail(List(1)) == Nil)
+    // assert(List.tail(List(1, 2)) == List(2))
+  // }
+
+  // test("EXERCISE 3.3 setHead") {
+    // assertThrows[RuntimeException] {
+      // List.setHead(1, Nil)
+    // }
+
+    // assert(List.setHead(9, List(1)) == List(9))
+    // assert(List.setHead(9, List(1, 2)) == List(9, 2))
+  // }
+
+}
