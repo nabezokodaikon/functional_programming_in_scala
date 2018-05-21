4b764d9da04076dd2c2a62cc3dff23bdf78027ac
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-22 08:07:58 +0900

    Add List 4-7

--- src/test/scala/fpinscala/errorhandling/EitherSpec.scala
+++ src/test/scala/fpinscala/errorhandling/EitherSpec.scala
@@ -3,4 +3,11 @@ package fpinscala.errorhandling
 import org.scalatest.FunSuite
 
 class EitherSpec extends FunSuite {
+
+  test("List 4-6") {
+    import Either.mean
+    assert(mean(IndexedSeq[Double]()) == Left("mean of empty list!"))
+    assert(mean(IndexedSeq(0.1)) == Right(0.1 / 1))
+    assert(mean(IndexedSeq(0.1, 0.2)) == Right((0.1 + 0.2) / 2))
+  }
 }
