0932b232d82ccc5b1b8a868e5966f4f4f0b29146
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-21 13:22:03 +0900

    Add List 4-3

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,5 +1,9 @@
 package fpinscala.errorhandling
 
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
 object Option {
 
   def failingFn(i: Int): Int = {
