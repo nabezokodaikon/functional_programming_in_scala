0f929e9405a301fdf82a0b0093ea752833f4b955
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-23 07:08:50 +0900

    Add headOption function on List 5-2

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,6 +1,14 @@
 package fpinscala.laziness
 
-trait Stream[+A]
+trait Stream[+A] {
+
+  def headOption: Option[A] =
+    this match {
+      case Empty => None
+      case Cons(h, _) => Some(h())
+    }
+}
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -3,4 +3,10 @@ package fpinscala.laziness
 import org.scalatest.FunSuite
 
 class StreamSpec extends FunSuite {
+
+  test("List 5-2 headOption") {
+    assert(Stream[Int]().headOption == None)
+    assert(Stream(1).headOption == Some(1))
+    assert(Stream(1, 2).headOption == Some(1))
+  }
 }
