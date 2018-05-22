55648d956c24f57a91d8bacb66ccd8ed8a7b16fa
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-22 19:48:54 +0900

    Add List 4-10

--- src/main/scala/fpinscala/errorhandling/Either.scala
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -69,4 +69,23 @@ object Either {
 
   def sequence_2[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
     traverse(es)(x => x)
+
+}
+
+case class Person(name: Name, age: Age)
+sealed case class Name(value: String)
+sealed case class Age(value: Int)
+
+object Person {
+
+  def mkName(name: String): Either[String, Name] =
+    if (name == "" || name == null) Left("Name is empty.")
+    else Right(Name(name))
+
+  def mkAge(age: Int): Either[String, Age] =
+    if (age < 0) Left("Age is out of range.")
+    else Right(Age(age))
+
+  def mkPerson(name: String, age: Int): Either[String, Person] =
+    mkName(name).map2(mkAge(age))(Person(_, _))
 }
