7845af39af385a3c7fc18feb7db23adea9a41572
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-25 08:12:13 +0900

    Add List 6-3 nextInt

--- src/test/scala/fpinscala/state/StateSpec.scala
+++ src/test/scala/fpinscala/state/StateSpec.scala
@@ -3,4 +3,13 @@ package fpinscala.state
 import org.scalatest.FunSuite
 
 class StateSpec extends FunSuite {
+
+  test("List 6-3 nextInt") {
+    val rng = SimpleRNG(42)
+    val (n1, rng2) = rng.nextInt
+    val (n2, rng3) = rng2.nextInt
+    assert(n1 == rng.nextInt._1)
+    assert(n2 == rng2.nextInt._1)
+  }
+
 }
