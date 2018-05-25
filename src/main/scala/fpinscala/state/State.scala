3861c7190f308993df8b22a130ba333a48c1733d
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-25 11:54:15 +0900

    Add EXERCISE 6.4

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -4,6 +4,9 @@ trait RNG {
   def nextInt: (Int, RNG)
   def nonNegariveInt(rng: RNG): (Int, RNG)
   def double(rng: RNG): (Double, RNG)
+  def intDouble(rng: RNG): ((Int, Double), RNG)
+  def doubleInt(rng: RNG): ((Double, Int), RNG)
+  def double3(rng: RNG): ((Double, Double, Double), RNG)
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
@@ -23,6 +26,24 @@ case class SimpleRNG(seed: Long) extends RNG {
   def double(rng: RNG): (Double, RNG) = {
     val (i, r) = rng.nonNegariveInt(rng)
     (i / (Int.MaxValue.toDouble + 1), r)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
   }
 }
 
--- src/test/scala/fpinscala/state/StateSpec.scala
+++ src/test/scala/fpinscala/state/StateSpec.scala
@@ -30,4 +30,34 @@ class StateSpec extends FunSuite {
     assert(n1 != n2)
   }
 
+  test("EXERCISE 6.3 intDouble") {
+    val rng = SimpleRNG(42)
+    val ((int1, double1), rng2) = rng.intDouble(rng)
+    val ((int2, double2), rng3) = rng.intDouble(rng2)
+    val ((int3, double3), _) = rng.intDouble(rng)
+    assert(int1 != int2)
+    assert(double1 != double2)
+    assert(int1 == int3)
+    assert(double1 == double3)
+  }
+
+  test("EXERCISE 6.3 doubleInt") {
+    val rng = SimpleRNG(42)
+    val ((double1, int1), rng2) = rng.doubleInt(rng)
+    val ((double2, int2), rng3) = rng.doubleInt(rng2)
+    val ((double3, int3), _) = rng.doubleInt(rng)
+    assert(int1 != int2)
+    assert(double1 != double2)
+    assert(int1 == int3)
+    assert(double1 == double3)
+  }
+
+  test("EXERCISE 6.4 double3") {
+    val rng = SimpleRNG(42)
+    val ((d1a, d2a, d3a), rng2) = rng.double3(rng)
+    val ((d1b, d2b, d3b), rng3) = rng.double3(rng2)
+    val ((d1c, d2c, d3c), rng4) = rng.double3(rng)
+    assert(d1a != d1b && d2a != d2b && d3a != d3b)
+    assert(d1a == d1c && d2a == d2c && d3a == d3c)
+  }
 }
