31ef14b4d72d5732e086a6422cbf8032d307ce8f
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-28 19:36:06 +0900

    Add List 7-5

--- src/main/scala/fpinscala/parallelism/Par.scala
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -39,21 +39,30 @@ object Par {
 
   // 直ちにa値が得られる計算を作成。
   // 定数値を並列計算に昇格させる。
-  // def unit[A](a: A): Par[A] =
-  // Par[A](a)
+  def unit[A](a: A): Par[A] =
+    (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
 
   // 2つの並列計算の結果を2項関数で結合。
-  /*
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
-    a.flatMap(aa => b.map(bb => f(aa, bb)))
-  */
-  // def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
-  // a.flatMap(aa => b.map(bb => f(aa, bb)))
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
 
   // runによる並列評価の対象としてマーク。
   // この評価はrunによって強制されるまで実際には発生しない。
   def fork[A](a: => Par[A]): Par[A] =
-    a
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
 
   // 式aをrunによる並列評価のためにラッピング。
   // 並列評価の対象としてマークする。
