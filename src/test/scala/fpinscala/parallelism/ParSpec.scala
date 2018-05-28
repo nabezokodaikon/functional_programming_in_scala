f147e9a4ff4fabe4bfbd8eb1623c9e7a5bbe3e48
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-28 20:10:51 +0900

    Add test code for EXERCISE 7.4

--- src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -1,5 +1,6 @@
 package fpinscala.parallelism
 
+import java.util.concurrent._
 import org.scalatest.FunSuite
 
 class ParSpec extends FunSuite {
@@ -33,4 +34,11 @@ class ParSpec extends FunSuite {
     assert(sum_4(IndexedSeq(1, 2, 3, 4)) == Par(10))
   }
   */
+
+  test("EXERCISE 7.4") {
+    val es = Executors.newFixedThreadPool(1)
+    val par = Par.asyncF((a: Int) => a * 2)
+    var r = Par.run(es)(par(3))
+    assert(r.get == 6)
+  }
 }
