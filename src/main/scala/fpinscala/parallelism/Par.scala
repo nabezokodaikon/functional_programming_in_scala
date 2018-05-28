5582dccdabddd1513d0b56e2e782f0a498e10ae7
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-28 10:22:29 +0900

    Add List 7-1 sum

--- src/main/scala/fpinscala/parallelism/Par.scala
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -1,6 +1,15 @@
 package fpinscala.parallelism
 
 object Par {
+
+  def sum(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      sum(l) + sum(r)
+    }
+
 }
 
 object Examples {
--- src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -3,4 +3,11 @@ package fpinscala.parallelism
 import org.scalatest.FunSuite
 
 class ParSpec extends FunSuite {
+
+  test("List 7-1 sum") {
+    import Par.sum
+    assert(sum(IndexedSeq[Int]()) == 0)
+    assert(sum(IndexedSeq(1)) == 1)
+    assert(sum(IndexedSeq(1, 2, 3, 4)) == 10)
+  }
 }
