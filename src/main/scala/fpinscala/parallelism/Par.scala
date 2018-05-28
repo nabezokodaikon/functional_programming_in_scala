701eaba259ed8f72a2cdc0eba6650f06ac4ea576
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-28 10:57:53 +0900

    Add List 7-2 sum_2

--- src/main/scala/fpinscala/parallelism/Par.scala
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -1,5 +1,8 @@
 package fpinscala.parallelism
 
+case class Par[A](a: A) {
+}
+
 object Par {
 
   def sum(ints: IndexedSeq[Int]): Int =
@@ -10,6 +13,21 @@ object Par {
       sum(l) + sum(r)
     }
 
+  def unit[A](a: => A): Par[A] =
+    Par[A](a)
+
+  def get[A](a: Par[A]): A =
+    a.a
+
+  def sum_2(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      val sumL: Par[Int] = Par.unit(sum_2(l))
+      val sumR: Par[Int] = Par.unit(sum_2(r))
+      Par.get(sumL) + Par.get(sumR)
+    }
 }
 
 object Examples {
--- src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -10,4 +10,11 @@ class ParSpec extends FunSuite {
     assert(sum(IndexedSeq(1)) == 1)
     assert(sum(IndexedSeq(1, 2, 3, 4)) == 10)
   }
+
+  test("List 7-2 sum_2") {
+    import Par.sum_2
+    assert(sum_2(IndexedSeq[Int]()) == 0)
+    assert(sum_2(IndexedSeq(1)) == 1)
+    assert(sum_2(IndexedSeq(1, 2, 3, 4)) == 10)
+  }
 }
