ea58b3ab2eed01ca245b08a3f5ecf79a0a7c62c0
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-29 09:26:22 +0900

    Add List 7-11 (Can not compile)

--- src/main/scala/fpinscala/parallelism/Nonblocking.scala
+++ src/main/scala/fpinscala/parallelism/Nonblocking.scala
@@ -5,4 +5,26 @@ import java.util.concurrent.atomic.AtomicReference
 import language.implicitConversions
 
 object NonBlocking {
+
+  trait Future[A] {
+    private[parallelism] def apply(k: A => Unit): Unit
+  }
+
+  type Par[+A] = ExecutorService => Future[A]
+
+  object Par {
+    
+    def run[A](es: ExecutorService)(p: Par[A]): A = {
+      val ref = new AtomicReference[A]
+      val latch = new CountDownLatch(1)
+
+      p(es) { a =>
+        ref.set(a)
+        latch.countDown
+      }
+
+      latch.await
+      ref.get
+    }
+  }
 }
