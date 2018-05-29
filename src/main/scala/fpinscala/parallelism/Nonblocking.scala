ed55a552cbbf4eec2ef91748df783852e8d2fa3a
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-29 09:10:50 +0900

    Add initial NonBlocking files

---
+++ src/main/scala/fpinscala/parallelism/Nonblocking.scala
@@ -0,0 +1,8 @@
+package fpinscala.parallelism
+
+import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
+import java.util.concurrent.atomic.AtomicReference
+import language.implicitConversions
+
+object NonBlocking {
+}
---
+++ src/test/scala/fpinscala/parallelism/NonblockingSpec.scala
@@ -0,0 +1,7 @@
+package fpinscala.parallelism
+
+import org.scalatest.FunSuite
+
+class NonBlockingSpec extends FunSuite {
+}
+
