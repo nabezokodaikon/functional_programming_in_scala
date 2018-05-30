ae6a7b4df7cc6e5c85c851b53f99c98a526932a1
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-30 15:42:38 +0900

    Add List 8-1

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -1,1 +1,19 @@
 package fpinscala.testing
+
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+
+object ScalaCheck extends App {
+
+  /*
+   * List 8-1
+   */
+  val intList = Gen.listOf(Gen.choose(0, 100))
+  val prop =
+    forAll(intList)(ns => ns.reverse.reverse == ns) &&
+      forAll(intList)(ns => ns.headOption == ns.reverse.lastOption)
+  prop.check
+  val failingProp = forAll(intList)(ns => ns.reverse == ns)
+  failingProp.check
+
+}
