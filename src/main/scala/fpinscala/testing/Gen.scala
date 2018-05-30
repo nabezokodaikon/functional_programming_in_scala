c7306d73b73149f39d8504332a112a8ffe9be8b7
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-30 16:20:45 +0900

    Add EXERCISE 8.1

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -16,4 +16,26 @@ object ScalaCheck extends App {
     failingProp.check
   }
 
+  {
+    println("EXERCISE 8.1")
+    val a = forAll { l1: List[Int] =>
+      l1.sum == l1.reverse.sum
+    }
+    a.check
+
+    val bl = Gen.listOf(Gen.choose(0, 10))
+    val b = forAll(bl) { ns =>
+      ns.sum == ns.reverse.sum
+    }
+    b.check
+
+    val cl = Gen.listOf(Gen.choose(10, 10))
+    val c = forAll(cl) { ns =>
+      ns.headOption match {
+        case Some(h) => h * ns.size == ns.sum
+        case _ => ns.size == ns.sum
+      }
+    }
+    c.check
+  }
 }
