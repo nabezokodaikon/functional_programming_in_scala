72d0e144d8bb997bcdd1b2176adf29fd67988946
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-30 15:49:56 +0900

    Modify exercise expression

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -6,9 +6,7 @@ import org.scalacheck.Prop.forAll
 object ScalaCheck extends App {
 
   {
-    /*
-     * List 8-1
-     */
+    println("List 8-1")
     val intList = Gen.listOf(Gen.choose(0, 100))
     val prop =
       forAll(intList)(ns => ns.reverse.reverse == ns) &&
