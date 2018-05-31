51b446d9949c899ab590286987686c1e371a365e
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-05-31 14:55:37 +0900

    Add EXERCISE 8.4 choose

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -1,63 +1,40 @@
 package fpinscala.testing
 
-object ScalaCheck extends App {
-  import org.scalacheck.Gen
-  import org.scalacheck.Prop.forAll
-
-  {
-    println("List 8-1")
-    val intList = Gen.listOf(Gen.choose(0, 100))
-    val prop =
-      forAll(intList)(ns => ns.reverse.reverse == ns) &&
-        forAll(intList)(ns => ns.headOption == ns.reverse.lastOption)
-    prop.check
-    val failingProp = forAll(intList)(ns => ns.reverse == ns)
-    failingProp.check
-  }
-
-  {
-    println("EXERCISE 8.1")
-    val a = forAll { l1: List[Int] =>
-      l1.sum == l1.reverse.sum
-    }
-    a.check
-
-    val bl = Gen.listOf(Gen.choose(0, 10))
-    val b = forAll(bl) { ns =>
-      ns.sum == ns.reverse.sum
-    }
-    b.check
-
-    val cl = Gen.listOf(Gen.choose(10, 10))
-    val c = forAll(cl) { ns =>
-      ns.headOption match {
-        case Some(h) => h * ns.size == ns.sum
-        case _ => ns.size == ns.sum
-      }
-    }
-    c.check
-  }
-
-  {
-    println("EXERCISE 8.2")
-    val al = Gen.listOf(Gen.choose(0, 10))
-    val a = forAll(al) { ns =>
-      ns match {
-        case Nil => try { ns.max; false } catch { case _: UnsupportedOperationException => true }
-        case (h :: Nil) => ns.max == h
-        case _ => ns.exists(_ == ns.max)
-      }
-    }
-    a.check
-  }
-}
+import fpinscala.state.{ RNG, State }
 
 sealed trait Prop {
+  import Prop.{ FailedCase, SuccessCount }
 
-  def check: Boolean
+  // def check: Boolean
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
   // EXERCISE 8.3
-  def &&(p: Prop): Prop = new Prop {
-    def check = Prop.this.check && p.check
-  }
+  // def &&(p: Prop): Prop = new Prop {
+  // def check = Prop.this.check && p.check
+  // }
 }
+
+object Prop {
+
+  type FailedCase = String
+  type SuccessCount = Int
+
+}
+
+case class Gen[+A](sample: State[RNG, A])
+
+object Gen {
+
+  // EXERCISE 8.4
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegariveInt).map(n => start + n % (stopExclusive - start)))
+}
+
+// object Gen {
+
+// def listOf[A](a: Gen[A]): Gen[List[A]]
+// def listOf[A](n: Int, a: Gen[A]): Gen[List[A]]
+
+// def forAll[A](a: Gen[A])(f: A => Boolean): Prop
+// }
---
+++ src/main/scala/fpinscala/testingsample/ScalaCheckSample.scala
@@ -0,0 +1,54 @@
+package fpinscala.testingsample
+
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+
+object ScalaCheckSample extends App {
+
+  {
+    println("List 8-1")
+    val intList = Gen.listOf(Gen.choose(0, 100))
+    val prop =
+      forAll(intList)(ns => ns.reverse.reverse == ns) &&
+        forAll(intList)(ns => ns.headOption == ns.reverse.lastOption)
+    prop.check
+    val failingProp = forAll(intList)(ns => ns.reverse == ns)
+    failingProp.check
+  }
+
+  {
+    println("EXERCISE 8.1")
+    val a = forAll { l1: List[Int] =>
+      l1.sum == l1.reverse.sum
+    }
+    a.check
+
+    val bl = Gen.listOf(Gen.choose(0, 10))
+    val b = forAll(bl) { ns =>
+      ns.sum == ns.reverse.sum
+    }
+    b.check
+
+    val cl = Gen.listOf(Gen.choose(10, 10))
+    val c = forAll(cl) { ns =>
+      ns.headOption match {
+        case Some(h) => h * ns.size == ns.sum
+        case _ => ns.size == ns.sum
+      }
+    }
+    c.check
+  }
+
+  {
+    println("EXERCISE 8.2")
+    val al = Gen.listOf(Gen.choose(0, 10))
+    val a = forAll(al) { ns =>
+      ns match {
+        case Nil => try { ns.max; false } catch { case _: UnsupportedOperationException => true }
+        case (h :: Nil) => ns.max == h
+        case _ => ns.exists(_ == ns.max)
+      }
+    }
+    a.check
+  }
+}
