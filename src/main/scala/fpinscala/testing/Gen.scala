b006d42fd1d93b9182961daabdef1fef0f376b6f
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-01 10:19:54 +0900

    Add EXERCISE 8.9

--- src/main/scala/fpinscala/testing/Gen.scala
+++ src/main/scala/fpinscala/testing/Gen.scala
@@ -15,6 +15,32 @@ sealed trait Prop {
   // def &&(p: Prop): Prop = new Prop {
   // def check = Prop.this.check && p.check
   // }
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+
+  // EXERCISE 8.9
+  def &&(p: Prop): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Passed => p.run(max, n, rng)
+        case x => x
+      }
+  }
+
+  def ||(p: Prop): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Falsified(msg, _) => p.tag(msg).run(max, n, rng)
+        case x => x
+      }
+  }
+
+  def tag(msg: String): Prop = Prop {
+    (max, n, rng) =>
+      run(max, n, rng) match {
+        case Falsified(e, c) => Falsified(msg + "\n" + e, c)
+        case x => x
+      }
+  }
 }
 
 object Prop {
