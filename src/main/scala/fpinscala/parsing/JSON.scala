917a49c341974fa02a33cf44102702c77a3099c1
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-05 07:20:05 +0900

    Add List 9-3

--- src/main/scala/fpinscala/parsing/JSON.scala
+++ src/main/scala/fpinscala/parsing/JSON.scala
@@ -6,3 +6,15 @@ def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = {
   val spaces = char(' ').many.slice
 }
 */
+
+// List 9-3
+trait JSON
+
+object JSON {
+  case object JNULL extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class jObject(get: Map[String, JSON]) extends JSON
+}
