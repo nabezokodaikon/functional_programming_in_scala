60e8f4b593dcc8ed861b3f1a23d72fac9139be66
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-06 13:20:47 +0900

    Add List 10-2 stringMonoid

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -6,3 +6,11 @@ trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
+
+object Monoid {
+
+  val stringMonoid = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+    val zero = ""
+  }
+}
--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -3,4 +3,11 @@ package fpinscala.monoids
 import org.scalatest.FunSuite
 
 class MonoidSpec extends FunSuite {
+
+  test("List 10-2 stringMonoid") {
+    import Monoid.stringMonoid.op
+    val a = op(op("a", "b"), "c")
+    val b = op("a", op("b", "c"))
+    assert(a == b)
+  }
 }
