3517a2dae7d80f1669a05558829ac3131e977b5f
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-07 14:16:06 +0900

    Add EXERCISE 10.13 TreeFoldable

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -189,4 +189,28 @@ object Monoid {
     override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B =
       as.foldLeft(z)(f)
   }
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object TreeFoldable extends Foldable[Tree] {
+    override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B =
+      as match {
+        case Leaf(a) => f(a)
+        case Branch(l, r) => mb.op(foldMap(l)(f)(mb), foldMap(r)(f)(mb))
+      }
+
+    override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B =
+      as match {
+        case Leaf(a) => f(z, a)
+        case Branch(l, r) => foldLeft(r)(foldLeft(l)(z)(f))(f)
+      }
+
+    override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B) =
+      as match {
+        case Leaf(a) => f(a, z)
+        case Branch(l, r) => foldRight(l)(foldRight(r)(z)(f))(f)
+      }
+  }
 }
