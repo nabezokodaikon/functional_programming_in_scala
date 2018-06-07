dd4fe5a2102c2b9ca8d071fdc99d703a6e587af9
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-08 08:42:15 +0900

    Add List 11-8 Monad

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -57,3 +57,15 @@ object Mon {
       o flatMap f
   }
 }
+
+// List 11-8
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a, b)))
+}
