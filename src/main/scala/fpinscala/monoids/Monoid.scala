3f8d6279f29ce820ce9f46590b940a60e1f29c5d
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-07 14:49:47 +0900

    Add EXERCISE 10.17 functionMonoid

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -254,4 +254,14 @@ object Monoid {
             acc.updated(k, V.op(a.getOrElse(k, V.zero), b.getOrElse(k, V.zero)))
         }
     }
+
+  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] = {
+    new Monoid[A => B] {
+      def op(f: A => B, g: A => B) =
+        a => B.op(f(a), g(a))
+
+      val zero: A => B =
+        a => B.zero
+    }
+  }
 }
