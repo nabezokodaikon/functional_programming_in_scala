2c47507127fbab8c71505f848da0cfc36af87096
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-07 15:12:18 +0900

    Add initial Monad codes

---
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -0,0 +1,9 @@
+package fpinscala
+package monads
+
+import parsing._
+import testing._
+import parallelism._
+import state._
+import parallelism.Par._
+import language.higherKinds
---
+++ src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -0,0 +1,13 @@
+package fpinscala
+package monads
+
+import org.scalatest.FunSuite
+import parsing._
+import testing._
+import parallelism._
+import state._
+import parallelism.Par._
+import language.higherKinds
+
+class MonadSpec extends FunSuite {
+}
