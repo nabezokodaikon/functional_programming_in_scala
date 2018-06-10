7c36588f35ad389559beda4d73753dffba554454
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-10 18:22:33 +0900

    Add EXERCISE 12.2

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -11,6 +11,7 @@ import language.implicitConversions
 // List 12-1
 trait Applicative[F[_]] extends Functor[F] {
   // プリミティブコンビネータ
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B]
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
   def unit[A](a: => A): F[A]
 
@@ -34,4 +35,17 @@ trait Applicative[F[_]] extends Functor[F] {
   // EXERCISE 12.1 product
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((_, _))
+
+  // EXERCISE 12.2 map2ViaApply
+  def map2ViaApply[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
+
+  // EXERCISE 12.2 mapViaApply
+  def mapViaApply[A, B](fa: F[A])(f: A => B): F[B] =
+    apply(unit(f))(fa)
+
+  // EXERCISE 12.2 applyViaMap2
+  def applyViaMap2[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)(_(_))
+  // map2(fab, fa)((a, b) => a(b))
 }
