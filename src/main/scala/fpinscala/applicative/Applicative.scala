ca399cfeed1d34dc74d993e3aa77de2f30d50860
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-10 17:40:59 +0900

    Add List 12-1

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -7,3 +7,19 @@ import State._
 import monoids._
 import language.higherKinds
 import language.implicitConversions
+
+// List 12-1
+trait Applicative[F[_]] extends Functor[F] {
+  // プリミティブコンビネータ
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def unit[A](a: => A): F[A]
+
+  // 派生コンビネータ
+  // mapはunitとmap2を使って実装できる。
+  // unit(())がダミー値()でunitを呼び出している。
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    map2(fa, unit(()))((a, _) => f(a))
+
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+}
