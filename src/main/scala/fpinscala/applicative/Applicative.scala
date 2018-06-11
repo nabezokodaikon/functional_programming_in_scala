295e88281e9fc95b3e92d2bfd8325d9add54fdce
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-11 14:09:23 +0900

    Remove unused code

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -10,9 +10,6 @@ import language.implicitConversions
 
 // List 12-1
 trait Applicative[F[_]] extends Functor[F] {
-  // プリミティブコンビネータ
-  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B]
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
   def unit[A](a: => A): F[A]
 
   // 派生コンビネータ
@@ -37,7 +34,7 @@ trait Applicative[F[_]] extends Functor[F] {
     map2(fa, fb)((_, _))
 
   // EXERCISE 12.2 map2ViaApply
-  def map2ViaApply[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     apply(map(fa)(f.curried))(fb)
 
   // EXERCISE 12.2 mapViaApply
@@ -45,7 +42,7 @@ trait Applicative[F[_]] extends Functor[F] {
     apply(unit(f))(fa)
 
   // EXERCISE 12.2 applyViaMap2
-  def applyViaMap2[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
     map2(fab, fa)(_(_))
   // map2(fab, fa)((a, b) => a(b))
 
