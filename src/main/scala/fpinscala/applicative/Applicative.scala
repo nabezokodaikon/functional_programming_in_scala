1dac6d29ca321755168c21539741fcceb69d8f2b
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-11 15:07:06 +0900

    Add EXERCISE 12.6 validationApplicative

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -103,9 +103,26 @@ object Applicative {
     override def map2[A, B, C](a: Stream[A], b: Stream[B])(f: (A, B) => C): Stream[C] =
       a zip b map f.tupled
   }
+
+  // EXERCISE 12.6
+  def validationApplicative[E]: Applicative[({ type f[x] = Validation[E, x] })#f] =
+    new Applicative[({ type f[x] = Validation[E, x] })#f] {
+
+      def unit[A](a: => A) = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C) =
+        (fa, fb) match {
+          case (Success(a), Success(b)) => Success(f(a, b))
+          case (Failure(h1, t1), Failure(h2, t2)) => Failure(h1, t1 ++ Vector(h2) ++ t2)
+          case (e @ Failure(_, _), _) => e
+          // case (Failure(a, b), _) => Failure(a, b)
+          case (_, e @ Failure(_, _)) => e
+          // case (_, Failure(a, b)) => Failure(a, b)
+        }
+    }
 }
 
-// list 12-6
+// List 12-6
 sealed trait Validation[+E, +A]
 
 case class Failure[E](head: E, tail: Vector[E] = Vector()) extends Validation[E, Nothing]
--- src/test/scala/fpinscala/applicative/ApplicativeSpec.scala
+++ src/test/scala/fpinscala/applicative/ApplicativeSpec.scala
@@ -12,4 +12,20 @@ class ApplicativeSpec extends FunSuite {
     assert(eitherMonad.flatMap(Right(1))(a => Right(a * 2)) == Right(2))
     assert(eitherMonad.flatMap(Left(1))(a => a) == Left(1))
   }
+
+  test("EXERCISE 12.6 validationApplicative") {
+    import Applicative.validationApplicative
+
+    assert(validationApplicative.unit(1) == Success(1))
+
+    assert(validationApplicative.map2(Success(1), Success(2))((a, b) => a + b) == Success((3)))
+    assert(validationApplicative.map2(Failure(1, Vector(2, 3)), Failure(4, Vector(5, 6)))((a, b) => (a, b)) ==
+      Failure(1, Vector(2, 3, 4, 5, 6)))
+
+    assert(validationApplicative.map2(Success(1), Failure(4, Vector(5, 6)))((a, b) => (a, b)) ==
+      Failure(4, Vector(5, 6)))
+
+    assert(validationApplicative.map2(Failure(1, Vector(2, 3)), Success(4))((a, b) => (a, b)) ==
+      Failure(1, Vector(2, 3)))
+  }
 }
