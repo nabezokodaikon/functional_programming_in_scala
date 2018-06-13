81fb6320a1a8d11216484bf4bc4f35aa98bfa5c3
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-13 18:09:52 +0900

    Add EXERCISE 12.18 fuse

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -305,6 +305,10 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] { self =>
       case (b, Nil) => ((None, b), Nil)
       case (b, a :: as) => ((Some(a), b), as)
     })._1
+
+  // EXERCISE 12.18 fuse
+  def fuse[G[_], H[_], A, B](fa: F[A])(f: A => G[B], g: A => H[B])(implicit G: Applicative[G], H: Applicative[H]): (G[F[B]], H[F[B]]) =
+    traverse[({ type f[x] = (G[x], H[x]) })#f, A, B](fa)(a => (f(a), g(a)))(G product H)
 }
 
 object Traverse {
