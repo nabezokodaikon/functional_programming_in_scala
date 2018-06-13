92cf67013c140c917a4d7aa707bb685b4a4ecdba
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-13 18:02:55 +0900

    Add List 12-7 zipL and zipR

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -291,6 +291,20 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] { self =>
       case (a, Nil) => sys.error("zip: Incompatible shapes.")
       case (a, b :: bs) => ((a, b), bs)
     })._1
+
+  // List 12-17 zipL
+  def zipL[A, B](fa: F[A], fb: F[B]): F[(A, Option[B])] =
+    (mapAccum(fa, toList(fb)) {
+      case (a, Nil) => ((a, None), Nil)
+      case (a, b :: bs) => ((a, Some(b)), bs)
+    })._1
+
+  // List 12-17 zipR
+  def zipR[A, B](fa: F[A], fb: F[B]): F[(Option[A], B)] =
+    (mapAccum(fb, toList(fa)) {
+      case (b, Nil) => ((None, b), Nil)
+      case (b, a :: as) => ((Some(a), b), as)
+    })._1
 }
 
 object Traverse {
