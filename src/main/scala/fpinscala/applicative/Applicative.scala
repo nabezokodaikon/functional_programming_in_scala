6e3a32338749acf1ecfc7348ab57d248d4a78a93
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-13 18:29:47 +0900

    Add EXERCISE 12.20 composeM

--- src/main/scala/fpinscala/applicative/Applicative.scala
+++ src/main/scala/fpinscala/applicative/Applicative.scala
@@ -132,6 +132,16 @@ object Monad {
     override def flatMap[A, B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
       st flatMap f
   }
+
+  // EXERCISE 12.20 composeM
+  def composeM[G[_], H[_]](implicit G: Monad[G], H: Monad[H], T: Traverse[H]): Monad[({ type f[x] = G[H[x]] })#f] =
+    new Monad[({ type f[x] = G[H[x]] })#f] {
+
+      def unit[A](a: => A): G[H[A]] = G.unit(H.unit(a))
+
+      override def flatMap[A, B](mna: G[H[A]])(f: A => G[H[B]]): G[H[B]] =
+        G.flatMap(mna)(na => G.map(T.traverse(na)(f))(H.join))
+    }
 }
 
 object Applicative {
