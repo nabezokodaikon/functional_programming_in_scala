426bf8ed4e6636ba8930c04376fc91726083c2b4
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-14 23:01:10 +0900

    Add List 10-1

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -1,1 +1,24 @@
 package fpinscala.monoids
+
+// List 10-1
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+}
+
+// trait Foldable[F[_]]
+
+// object ListFoldable[F[_]] {
+// }
+
+// object IndexedSeqFoldable[F[_]] {
+// }
+
+// object StreamFoldable[F[_]] {
+// }
+
+// object OptionFoldable[F[_]] {
+// }
--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -3,4 +3,16 @@ package fpinscala.monoids
 import org.scalatest.FunSuite
 
 class MonoidSpec extends FunSuite {
+
+  test("List 10-1") {
+    val m = new Monoid[Int] {
+      def op(a1: Int, a2: Int) =
+        a1 + a2
+
+      def zero = 0
+    }
+
+    import m._
+    assert(op(1, op(2, 3)) == op(op(1, 2), 3))
+  }
 }
