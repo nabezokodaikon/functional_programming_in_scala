f4b900728bee454f34d4d07afb1a19ded4e3a304
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-14 22:54:16 +0900

    Fix monoid initial code

--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -4,20 +4,3 @@ import org.scalatest.FunSuite
 
 class MonoidSpec extends FunSuite {
 }
-
-object Monoid {
-}
-
-trait Foldable[F[_]]
-
-object ListFoldable[F[_]] {
-}
-
-object IndexedSeqFoldable[F[_]] {
-}
-
-object StreamFoldable[F[_]] {
-}
-
-object OptionFoldable[F[_]] {
-}
