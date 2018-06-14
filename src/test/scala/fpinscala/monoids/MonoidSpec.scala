599d1f5c77accedd4218bf22dddaeb8e9e722ed0
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-14 23:16:18 +0900

    Add EXERCISE 10.1

--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -15,4 +15,12 @@ class MonoidSpec extends FunSuite {
     import m._
     assert(op(1, op(2, 3)) == op(op(1, 2), 3))
   }
+
+  test("EXERCISE 10.1") {
+    import Monoid._
+    assert(intAddition.op(1, intAddition.op(2, 3)) == intAddition.op(intAddition.op(1, 2), 3))
+    assert(intMultiplication.op(1, intMultiplication.op(2, 3)) == intMultiplication.op(intMultiplication.op(1, 2), 3))
+    assert(booleanOr.op(true, booleanOr.op(false, false)) == booleanOr.op(booleanOr.op(true, false), false))
+    assert(booleanAnd.op(true, booleanAnd.op(false, false)) == booleanAnd.op(booleanAnd.op(true, false), false))
+  }
 }
