9123923617f30b919d7203f892ef2c81f82c0f08
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-14 09:14:51 +0900

    Add List 13-1

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -3,3 +3,36 @@ package fpinscala.iomonad
 import language.postfixOps
 import language.higherKinds
 import scala.io.StdIn.readLine
+
+// List 13-1
+case class Player(name: String, score: Int)
+
+object Player {
+
+  def contest(p1: Player, p2: Player): Unit =
+    if (p1.score > p2.score)
+      println(s"${p1.name} is the winner!")
+    else if (p2.score > p1.score)
+      println(s"${p2.name} is the winner!")
+    else
+      println("It's a draw.")
+
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score) Some(p1)
+    else if (p2.score > p1.score) Some(p2)
+    else None
+
+  def contest2(p1: Player, p2: Player): Unit =
+    winner(p1, p2) match {
+      case Some(Player(name, _)) => println(s"${name} is the winner!")
+      case None => println("It's a draw.")
+    }
+
+  def winnerMsg(p: Option[Player]): String =
+    p map {
+      case Player(name, _) => s"${name} is the winner!"
+    } getOrElse "It's a draw."
+
+  def contest3(p1: Player, p2: Player): Unit =
+    println(winnerMsg(winner(p1, p2)))
+}
--- src/test/scala/fpinscala/iomonad/IOSpec.scala
+++ src/test/scala/fpinscala/iomonad/IOSpec.scala
@@ -3,4 +3,13 @@ package fpinscala.iomonad
 import org.scalatest.FunSuite
 
 class IOSpec extends FunSuite {
+
+  test("List 13-1") {
+    val p1 = Player("taro", 10)
+    val p2 = Player("jiro", 5)
+    Player.contest(p1, p2)
+    Player.contest2(p1, p2)
+    Player.contest3(p1, p2)
+  }
+
 }
