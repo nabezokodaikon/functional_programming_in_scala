5bef9962ff1cdb2d3a4fffed6198aa874671b8ae
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-14 22:46:54 +0900

    Add initial monoids code

--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -4,3 +4,20 @@ import org.scalatest.FunSuite
 
 class MonoidSpec extends FunSuite {
 }
+
+object Monoid {
+}
+
+trait Foldable[F[_]]
+
+object ListFoldable[F[_]] {
+}
+
+object IndexedSeqFoldable[F[_]] {
+}
+
+object StreamFoldable[F[_]] {
+}
+
+object OptionFoldable[F[_]] {
+}
