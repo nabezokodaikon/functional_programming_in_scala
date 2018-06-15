38dfc65f2bc563f48f3845419912d02423bfa93d
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-15 21:38:57 +0900

    Add List 11-1

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -7,3 +7,10 @@ import parallelism._
 import state._
 import parallelism.Par._
 import language.higherKinds
+
+trait Functor[F[_]] {
+
+  // List 11-1
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+}
