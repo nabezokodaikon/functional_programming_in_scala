5ddf98e1ecafdae3b2b53a52cdec083b87d0985e
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-15 15:05:02 +0900

    Add Foldable trait

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -114,7 +114,16 @@ object Monoid {
   }
 }
 
-// trait Foldable[F[_]]
+trait Foldable[F[_]] {
+
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
+
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B
+
+  def concatenate[A](as: F[A])(m: Monoid[A]): A
+}
 
 // object ListFoldable[F[_]] {
 // }
