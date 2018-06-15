764d0b9b84abda699e13731a02e24d439df29564
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-15 21:42:14 +0900

    Add List 11-2

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -14,3 +14,12 @@ trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
 }
+
+object Functor {
+
+  // List 11-2
+  val listFunctor = new Functor[List] {
+    def map[A, B](as: List[A])(f: A => B): List[B] = as map f
+  }
+
+}
--- src/test/scala/fpinscala/monads/MonadSpec.scala
+++ src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -10,4 +10,9 @@ import parallelism.Par._
 import language.higherKinds
 
 class MonadSpec extends FunSuite {
+
+  test("List 11-2") {
+    val f = Functor.listFunctor
+    assert(f.map(List(1, 2, 3))(a => a * 2) == List(2, 4, 6))
+  }
 }
