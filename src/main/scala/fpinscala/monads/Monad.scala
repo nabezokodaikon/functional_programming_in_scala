9490850ff2ee9ff8b09d64f8d920b471df00e2c0
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-15 21:50:46 +0900

    Add List 11-3 distribute(unzip)

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -13,6 +13,10 @@ trait Functor[F[_]] {
   // List 11-1
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
+  // List 11-3
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(_._1), map(fab)(_._2))
+
 }
 
 object Functor {
--- src/test/scala/fpinscala/monads/MonadSpec.scala
+++ src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -15,4 +15,10 @@ class MonadSpec extends FunSuite {
     val f = Functor.listFunctor
     assert(f.map(List(1, 2, 3))(a => a * 2) == List(2, 4, 6))
   }
+
+  test("List 11-3 distribute(unzip)") {
+    val f = Functor.listFunctor
+    val l = List((1, 2), (3, 4), (5, 6))
+    assert(f.distribute(l) == (List(1, 3, 5), List(2, 4, 6)))
+  }
 }
