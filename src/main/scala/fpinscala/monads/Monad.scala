3c63b55930b054839db16d5ddef808e27341b89c
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-17 13:56:46 +0900

    Add EXERCISE 11.20

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -168,3 +168,19 @@ object StateMonad {
       _ <- setState(n + 1)
     } yield (n, a) :: xs).run(0)._1.reverse
 }
+
+// EXERCISE 11.20
+case class Reader[R, A](run: R => A)
+
+object Reader {
+
+  def readerMonad[R] = new Monad[({ type f[x] = Reader[R, x] })#f] {
+
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+
+    def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+      Reader(r => f(st.run(r)).run(r))
+  }
+
+  def ask[R]: Reader[R, R] = Reader(r => r)
+}
