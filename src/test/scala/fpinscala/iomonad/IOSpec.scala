2f0f5a80a16864a4ca0a3467dc1014c09b857176
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-18 04:54:12 +0900

    Add List 13-1

--- src/test/scala/fpinscala/iomonad/IOSpec.scala
+++ src/test/scala/fpinscala/iomonad/IOSpec.scala
@@ -3,4 +3,18 @@ package fpinscala.iomonad
 import org.scalatest.FunSuite
 
 class IOSpec extends FunSuite {
+
+  test("List 13-1") {
+    Player.contest(Player("taro", 100), Player("jiro", 90))
+    Player.contest(Player("taro", 50), Player("jiro", 100))
+    Player.contest(Player("taro", 50), Player("jiro", 50))
+
+    Player.contest2(Player("taro", 100), Player("jiro", 90))
+    Player.contest2(Player("taro", 50), Player("jiro", 100))
+    Player.contest2(Player("taro", 50), Player("jiro", 50))
+
+    Player.contest3(Player("taro", 100), Player("jiro", 90))
+    Player.contest3(Player("taro", 50), Player("jiro", 100))
+    Player.contest3(Player("taro", 50), Player("jiro", 50))
+  }
 }
