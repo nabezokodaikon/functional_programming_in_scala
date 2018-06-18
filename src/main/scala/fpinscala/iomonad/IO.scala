daf8283b74231205a24171e056fc03e7092b4d3f
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-18 19:56:10 +0900

    Add EXERCISE 13.1

--- src/main/scala/fpinscala/iomonad/IO.scala
+++ src/main/scala/fpinscala/iomonad/IO.scala
@@ -310,10 +310,25 @@ object IO2c {
 object IO3 {
 
   // List 13-14
-  sealed trait Free[F[_], A]
+  sealed trait Free[F[_], A] {
+
+    def flatMap[B](f: A => Free[F, B]): Free[F, B] =
+      FlatMap(this, f)
+
+    def map[B](f: A => B): Free[F, B] =
+      flatMap(f andThen (Return(_)))
+  }
+
   case class Return[F[_], A](a: A) extends Free[F, A]
   case class Suspend[F[_], A](s: F[A]) extends Free[F, A]
   case class FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+
+  // EXERCISE 13.1
+  def freeMonad[F[_]]: Monad[({ type f[a] = Free[F, a] })#f] =
+    new Monad[({ type f[a] = Free[F, a] })#f] {
+      def unit[A](a: => A) = Return(a)
+      def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]) = fa flatMap f
+    }
 }
 
 object Main extends App {
