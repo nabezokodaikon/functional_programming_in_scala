4cd7cf92a8c3099f64f56654d824ef2a8b45b570
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-27 19:27:25 +0900

    Add List 15-1

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -3,3 +3,21 @@ package fpinscala.streamingio
 import language.implicitConversions
 import language.higherKinds
 import language.postfixOps
+import fpinscala.iomonad.IO1.IO
+
+object ImperativeAndLazyIO {
+
+  // List 15-1
+  def linesGt5(filename: String): IO[Boolean] = IO {
+    val src = scala.io.Source.fromFile(filename)
+    try {
+      var count = 0
+      val lines: Iterator[String] = src.getLines
+      while (count <= 5 && lines.hasNext) {
+        lines.next // 次の要素に進むという副作用がある。
+        count += 1
+      }
+      count > 5
+    } finally src.close
+  }
+}
--- src/test/scala/fpinscala/streamingio/StreamingIOSpec.scala
+++ src/test/scala/fpinscala/streamingio/StreamingIOSpec.scala
@@ -3,5 +3,11 @@ package fpinscala.streamingio
 import org.scalatest.FunSuite
 
 class StreamingIOSpec extends FunSuite {
+
+  test("List 15-1 linesGt5") {
+    import ImperativeAndLazyIO._
+    assert(linesGt5("""resource/List15-1_01.txt""").run == true)
+    assert(linesGt5("""resource/List15-1_02.txt""").run == false)
+  }
 }
 
