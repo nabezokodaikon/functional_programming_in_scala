1f2a20e60fbbeff83ea5c9a8d22fbabadc7c6542
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-27 20:08:39 +0900

    Add Example code

--- src/test/scala/fpinscala/streamingio/StreamingIOSpec.scala
+++ src/test/scala/fpinscala/streamingio/StreamingIOSpec.scala
@@ -9,5 +9,12 @@ class StreamingIOSpec extends FunSuite {
     assert(linesGt5("""resource/List15-1_01.txt""").run == true)
     assert(linesGt5("""resource/List15-1_02.txt""").run == false)
   }
+
+  test("Examples 01") {
+    val lines: Stream[String] = Stream("a", "b", "a", " ", "c", "a")
+    assert(lines.zipWithIndex.exists(_._2 + 1 >= 4) == true)
+    assert(lines.filter(!_.trim.isEmpty).zipWithIndex.exists(_._2 + 1 >= 4) == true)
+    assert(lines.filter(!_.trim.isEmpty).take(4).map(_.head).indexOfSlice("c".toList) == 3)
+  }
 }
 
