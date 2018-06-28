61836766743e532228c8e90f6c6a5fcb970254b4
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-29 06:55:17 +0900

    Add EXERCISE 15.5 |>

--- src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -65,6 +65,19 @@ object SimpleStreamTransducers {
 
       go(this)
     }
+
+    // EXERCISE 15.5
+    def |>[O2](p2: Process[O, O2]): Process[I, O2] =
+      p2 match {
+        case Halt() => Halt()
+        case Emit(h, t) => Emit(h, this |> t)
+        case Await(f) =>
+          this match {
+            case Emit(h, t) => t |> f(Some(h))
+            case Halt() => Halt() |> f(None)
+            case Await(g) => Await((i: Option[I]) => g(i) |> p2)
+          }
+      }
   }
 
   object Process {
