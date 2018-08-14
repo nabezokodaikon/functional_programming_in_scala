5836df093116c08874fc6a007b599f370e1aec4f
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-15 06:02:51 +0900

    Add EXERCISE 10.1

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -19,4 +19,28 @@ object Monoid {
     def op(a1: List[A], a2: List[A]) = a1 ++ a2
     val zero = Nil
   }
+
+  // EXERCISE 10.1
+  val intAddition = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 + a2
+    val zero = 0
+  }
+
+  // EXERCISE 10.1
+  val intMultiplication = new Monoid[Int] {
+    def op(a1: Int, a2: Int) = a1 * a2
+    val zero = 1
+  }
+
+  // EXERCISE 10.1
+  val booleanOr = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 || a2
+    val zero = false
+  }
+
+  // EXERCISE 10.1
+  val booleanAnd = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean) = a1 && a2
+    val zero = true
+  }
 }
--- src/test/scala/fpinscala/monoids/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoids/MonoidSpec.scala
@@ -1,11 +1,37 @@
 package fpinscala.monoids
 
-import org.scalatest._
+import org.scalatest.FunSuite
 
-class MonoidSpec extends FlatSpec with Matchers {
+class MonoidSpec extends FunSuite {
+  import fpinscala.monoids._
 
-  it should "Example" in {
+  test("EXERCISE 10.1 intAddition") {
+    import Monoid.intAddition._
 
-    1 should be(1)
+    assert(op(op(1, 2), 3) == op(1, op(2, 3)))
+    assert(op(zero, 1) == 1)
+  }
+
+  test("EXERCISE 10.1 intMultiplication") {
+    import Monoid.intMultiplication._
+
+    assert(op(op(1, 2), 3) == op(1, op(2, 3)))
+    assert(op(zero, 1) == 1)
+  }
+
+  test("EXERCISE 10.1 booleanOr") {
+    import Monoid.booleanOr._
+
+    assert(op(op(true, false), true) == op(true, op(false, true)))
+    assert(op(zero, true) == true)
+    assert(op(zero, false) == false)
+  }
+
+  test("EXERCISE 10.1 booleanAnd") {
+    import Monoid.booleanAnd._
+
+    assert(op(op(true, false), true) == op(true, op(false, true)))
+    assert(op(zero, true) == true)
+    assert(op(zero, false) == false)
   }
 }
