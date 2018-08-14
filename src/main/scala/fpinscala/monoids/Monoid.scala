2acb82c3958d61f2173f264d527c0076f96efc41
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-08-15 05:37:07 +0900

    Add List 10-2

--- src/main/scala/fpinscala/monoids/Monoid.scala
+++ src/main/scala/fpinscala/monoids/Monoid.scala
@@ -5,3 +5,12 @@ trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
 }
+
+object Monoid {
+
+  // List 10-2
+  val stringMonoid = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+    val zero = ""
+  }
+}
